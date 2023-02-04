991a1594137dae17fd04243672a4bae7889e65e0
RakeshM <rakeshe.mishra@kone.com>
2023-02-04 10:08:05 +0530

    added get user api

--- src/screens/HomeDetails.tsx
+++ src/screens/HomeDetails.tsx
@@ -1,11 +1,29 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import colors from '../utils/colors';
 
 const HomeDetails: FC = () => {
+  const getUser = () => {
+    const userUrl = 'https://gorest.co.in/public/v2/users';
+    fetch(userUrl)
+      .then(response => response.json())
+      .then(json => {
+        // console.log('user:', json);
+        return json.user;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Home details screeen</Text>
+      <Text>User details</Text>
     </View>
   );
 };
